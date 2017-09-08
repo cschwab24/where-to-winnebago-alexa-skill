d89d6c6dc6bc41395deb74837b3686c621cbe1df
Christopher Schwab <cschwab24@gmail.com>
2017-09-08 10:13:24 -0400

    back functionality working

--- skill_files/lambdaFun/index.js
+++ skill_files/lambdaFun/index.js
@@ -21,13 +21,15 @@ exports.handler = function(event,context) {
       handleLaunchRequest(context);
     } else if (request.type === "IntentRequest") {
       if (request.intent.name === "PickStateIntent") {
-        handlePickStateIntent(request,context);
+        handlePickStateIntent(request,context,session);
       } else if (request.intent.name === "NhPlaceIntent") {
         handleNhPlaceIntent(request,context,session);
       } else if (request.intent.name === "CaActivityIntent") {
         handleCaActivityIntent(request,context,session);
       } else if (request.intent.name === "StartOverIntent") {
         handleStartOverIntent(request,context,session);
+      } else if (request.intent.name === "BackIntent") {
+        handleBackIntent(request,context,session);
       } else if (request.intent.name === "AMAZON.StopIntent" || request.intent.name === "AMAZON.CancelIntent") {
         context.succeed(buildResponse({
           speechText: "Good bye. ",
@@ -103,10 +105,11 @@ function handleLaunchRequest(context) {
   context.succeed(buildResponse(options));
 }
 
-function handlePickStateIntent(request,context) {
+function handlePickStateIntent(request,context,session) {
   let options = {};
   let state = request.intent.slots.State.value;
   let lc_State = state.toLowerCase();
+  options.session = session;
   if (lc_State == 'new hampshire' || state == 'california'){
     // options.speechText = `Let's go to ${state}`;
   }
@@ -114,14 +117,17 @@ function handlePickStateIntent(request,context) {
     case 'new hampshire':
       options.speechText = "Great, we think the granite state is pretty cool too. Would you like to hike a mountain or visit the seacoast?";
       options.imageUrl = "https://www.rocketbanner.com/images/states/new-hampshire/new-hampshire-vinyl-banners.jpg";
+      options.session.attributes.currentstate = "new hampshire";
       break;
     case 'california':
       options.speechText = "Okay, are you interested in family friendly activities or adults only?";
       options.imageUrl = "https://cdn.history.com/sites/2/2015/09/GettyImages-501880463.jpg";
+      options.session.attributes.currentstate = "california";
       break;
     default:
       options.speechText = "Please choose from either California or New Hampshire for this prototype.";
       options.imageUrl = "";
+      options.session.attributes.currentstate = "";
   }
   options.cardTitle = "Adventure Time!"
   options.cardContent = `Let's go to ${state}!`;
@@ -137,10 +143,12 @@ function handleNhPlaceIntent(request,context,session) {
     options.speechText = "Great, we’d suggest Mount Washington, the tallest peak east of the Mississippi. This mountain has an elevation of 6,289 feet and is home to some of the world’s wildest weather. You can stay at the nearby Twin Mountain KOA campground. An adventure card has been sent with an address, phone number, and additional information.";
     options.cardTitle = "Twin Mountain KOA campground"
     options.cardContent = "Amenities:\n\u202250 AMP Max\n\u202290’ Max Length\n\u2022Wi-Fi\n\u2022Cable TV\n\u2022Pool\n\u2022Dog Park\n\u2022Game Room\n\u2022General Store\nhttp://koa.com/campgrounds/twin-mountain/\n372 NH-115\nCarroll, NH 03598\n(603) 846-5559";
+    options.session.attributes.nhplace = "hike";
   } else if (nh_place == 'seacoast' || nh_place == 'visit the seacoast') {
     options.speechText = "Great, we recommend the Wakeda Campground located in Hampton Falls. It’s close to the beaches and a short drive from our home, the bustling seaside town of Portsmouth, NH.  An adventure card has been sent with the address, phone number, and additional information.";
     options.cardTitle = "Wakeda Campground";
     options.cardContent = "http://www.wakedacampground.com/ \n 294 Exeter Rd, Hampton Falls, NH 03844 \n (603) 772-5274";
+    options.session.attributes.nhplace = "seacoast";
   }
   options.endSession = false;
   context.succeed(buildResponse(options));
@@ -154,10 +162,12 @@ function handleCaActivityIntent(request,context,session) {
     options.speechText = "Everyone loves Disneyland, right? The closest place to park your RV near Disneyland is less than two miles away at the Anaheim RV Park. An adventure card has been sent with an address, phone number, and additional information.";
     options.cardTitle = "Disneyland"
     options.cardContent = "www.anaheimrvpark.com\n200 W Midway Dr\nAnaheim, CA 92805\n(714) 774-3860";
+    options.session.attributes.caactivity = "family";
   } else if (ca_activity == 'adults' || ca_activity == 'adults only') {
     options.speechText = "Rad! Drive your Winnebago over to the American River Resort. This resort also offers white water rafting excursions as one of their amenities. An adventure card has been sent to you with and address, phone number and additional information.";
     options.cardTitle = "American River Resort";
     options.cardContent = "http://www.americanriverresort.com/\n6019 New River Road\nColoma, CA 95613\n(530) 622-6700";
+    options.session.attributes.caactivity = "adult";
   }
   options.endSession = false;
   context.succeed(buildResponse(options));
@@ -165,8 +175,50 @@ function handleCaActivityIntent(request,context,session) {
 
 function handleStartOverIntent(request,context,session) {
   let options = {};
+  options.session = session;
   options.speechText =  "Let’s get you started on your next adventure. First let’s pick a state. Would you rather explore New Hampshire or California?";
   options.repromptText = "Would you rather explore New Hampshire or California?";
+  options.session.attributes.currentstate = "";
+  options.session.attributes.nhplace = "";
+  options.session.attributes.caactivity = "";
+  options.endSession = false;
+  context.succeed(buildResponse(options));
+}
+
+function handleBackIntent(request,context,session) {
+  let options = {};
+  options.session = session;
+  let currentstate = options.session.attributes.currentstate;
+  let nhplace = options.session.attributes.nhplace;
+  let caactivity = options.session.attributes.caactivity;
+  if (currentstate !== '') {
+    // we have picked a state
+    if (nhplace !== '') {
+      // we have picked a new hampshire place
+      options.speechText = "Would you like to hike a mountain or visit the seacoast?";
+      options.session.attributes.currentstate = "new hampshire";
+      options.session.attributes.nhplace = "";
+    } else if (caactivity !== '') {
+      // we have picked a california activity
+      options.speechText = "Are you interested in family friendly activities or adults only?";
+      options.session.attributes.currentstate = "california";
+      options.session.attributes.caactivity = "";
+    } else {
+      // we have picked a state, but have not picked a place/activity :: start over
+      options.speechText =  "Let’s get you started on your next adventure. First let’s pick a state. Would you rather explore New Hampshire or California?";
+      options.repromptText = "Would you rather explore New Hampshire or California?";
+      options.session.attributes.currentstate = "";
+      options.session.attributes.nhplace = "";
+      options.session.attributes.caactivity = "";
+    }
+  } else {
+    // we have not picked a state :: start over
+    options.speechText =  "Let’s get you started on your next adventure. First let’s pick a state. Would you rather explore New Hampshire or California?";
+    options.repromptText = "Would you rather explore New Hampshire or California?";
+    options.session.attributes.currentstate = "";
+    options.session.attributes.nhplace = "";
+    options.session.attributes.caactivity = "";
+  }
   options.endSession = false;
   context.succeed(buildResponse(options));
 }
